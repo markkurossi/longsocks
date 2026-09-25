31e19be123ceb39fbf5912417b37555677e51dd9
Markku Rossi <mtr@iki.fi>
2026-09-25 09:23:50 +0200

    SOCKS5 cleanup.

--- cmd/longsocksd/hosts.go
+++ cmd/longsocksd/hosts.go
@@ -153,16 +153,45 @@ func NewAppConn(conn net.Conn, cookie string) {
 	}
 	delete(appConnReqs, cookie)
 
+	// The SOCKS request information is sent by the client as soon as it has
+	// established a connection to the SOCKS server, and completed the
+	// authentication negotiations.  The server evaluates the request, and
+	// returns a reply formed as follows:
+	//
+	//      +----+-----+-------+------+----------+----------+
+	//      |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
+	//      +----+-----+-------+------+----------+----------+
+	//      | 1  |  1  | X'00' |  1   | Variable |    2     |
+	//      +----+-----+-------+------+----------+----------+
+	//
+	//   Where:
+	//
+	//        o  VER    protocol version: X'05'
+	//        o  REP    Reply field:
+	//           o  X'00' succeeded
+	//           o  X'01' general SOCKS server failure
+	//           o  X'02' connection not allowed by ruleset
+	//           o  X'03' Network unreachable
+	//           o  X'04' Host unreachable
+	//           o  X'05' Connection refused
+	//           o  X'06' TTL expired
+	//           o  X'07' Command not supported
+	//           o  X'08' Address type not supported
+	//           o  X'09' to X'FF' unassigned
+	//        o  RSV    RESERVED
+	//        o  ATYP   address type of following address
+	//           o  IP V4 address: X'01'
+	//           o  DOMAINNAME: X'03'
+	//           o  IP V6 address: X'04'
+	//        o  BND.ADDR       server bound address
+	//        o  BND.PORT       server bound port in network octet order
+	//
+	// Fields marked RESERVED (RSV) must be set to X'00'.
+
 	var reply [10]byte
 
 	reply[0] = 0x05
 	reply[3] = 0x01
-	reply[4] = 127
-	reply[5] = 0
-	reply[6] = 0
-	reply[7] = 1
-	reply[8] = 0
-	reply[9] = 22
 
 	_, err := appConn.Write(reply[:])
 	if err != nil {
